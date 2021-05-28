79e7bf0626ace20031a62c864c71c24b41db28ee
naolArega <na408498@gmail.com>
2021-05-28 18:38:58 +0300

    Containers, padding, rows and columns

--- lib/main.dart
+++ lib/main.dart
@@ -9,19 +9,90 @@ void main() {
 }
 
 class Home extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
+    int count = 0;
+    List<Widget> listOfWidgets = [];
+
+    for(int i = 0; i < 10; i++){
+      listOfWidgets.add(
+        Text('Item $i')
+      );
+    }
+
     return  Scaffold(
       appBar: AppBar(
         title: Text('Flutter App'),
         backgroundColor: Colors.greenAccent,
       ),
-      body: Center(
-        child: Image.asset('assets/cyberpunk_2077.jpg'),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  vertical: 20,
+                  horizontal: 30
+                ),
+                child: Text('This is a row'),
+              ),
+              Container(
+                margin: EdgeInsets.all(10),
+                child: Icon(
+                  Icons.account_circle
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10),
+                child: Icon(
+                  Icons.account_circle
+                ),
+              ),
+            ]
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: Text('This is a column'),
+              ),
+              Container(
+                color: Colors.deepPurpleAccent,
+                padding: EdgeInsets.all(20),
+                child: Text(
+                    'Item one',
+                  style: TextStyle(
+                    fontFamily: 'Pattaya',
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Container(
+                color: Colors.redAccent,
+                padding: EdgeInsets.all(20),
+                child: Text(
+                    'Item two',
+                  style: TextStyle(
+                    fontFamily: 'Pattaya',
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => print('hello world'),
-        child: Text('World'),
+        onPressed: () {
+          print('hello world');
+          listOfWidgets.clear();
+        },
+        child: Icon(
+          Icons.add,
+          size: 30,
+        ),
         backgroundColor: Colors.greenAccent,
       ),
     );
