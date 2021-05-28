044f1783ac3eca0f90fec85735f571179c209f26
naolArega <na408498@gmail.com>
2021-05-28 16:48:12 +0300

    Stateless widgets

--- lib/main.dart
+++ lib/main.dart
@@ -3,32 +3,40 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('An Actual Flutter App'),
-          backgroundColor: Colors.greenAccent,
-        ),
-        body: Center(
-          child: Column(
-            children: [
-              Text(
-                  'hello world',
+      home: Home(),
+    )
+  );
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+        backgroundColor: Colors.greenAccent,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text(
+                'hello world',
                 style: TextStyle(
                   fontSize: 20,
                   fontWeight: FontWeight.bold,
                   fontFamily: 'Pattaya',
                 )
-              ),
-              Text('i am naol')
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => print('hello world'),
-          child: Text('World'),
-          backgroundColor: Colors.greenAccent,
+            ),
+            Text('i am naol')
+          ],
         ),
       ),
-    )
-  );
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => print('hello world'),
+        child: Text('World'),
+        backgroundColor: Colors.greenAccent,
+      ),
+    );
+  }
 }
+
